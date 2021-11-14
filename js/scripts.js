bcd9929393be5e0dd3bbc01fdeb1d4954cfc7a55
Yaroky <alekno.yaroslav@gmail.com>
2021-11-14 17:34:31 +0500

    тест

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (error === 0) {
       form.classList.add('_sending');
-      let response = await fetch("sendmail.php", {
+      let response = await fetch('sendmail.php', {
         method: "POST",
         body: formData,
       });
--- sendmail.php
+++ sendmail.php
@@ -3,6 +3,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 
